b6ea2d66ac0c1e1b407ddd69583598cfa9e80e85
Huy Nguyen <huyndinh@gmail.com>
2018-09-29 16:53:56 -0700

    Added card matching mode

---
+++ Machismo/Machismo/Model/CardMatchingGame.h
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.h
+//  Machismo
+//
+//  Created by Huy Nguyen on 9/29/18.
+//  Copyright © 2018 CS193p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CardMatchingGame : NSObject
+
+@end
---
+++ Machismo/Machismo/Model/CardMatchingGame.m
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.m
+//  Machismo
+//
+//  Created by Huy Nguyen on 9/29/18.
+//  Copyright © 2018 CS193p. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@implementation CardMatchingGame
+
+@end
